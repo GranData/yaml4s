a7b6d6ac7b6ebdb8d4df06f6857d252cecdb0b8c
Gustavo Ajzenman <gustavoajz@gmail.com>
2015-08-15 20:52:13 -0300

    Implement yaml package object

--- src/main/scala/com/grandata/yaml/package.scala
+++ src/main/scala/com/grandata/yaml/package.scala
@@ -1,19 +1,21 @@
 package com.grandata
 
+import org.json4s.{Formats, DefaultFormats, JValue, JsonInput}
+
 /**
  * Created by gustavo on 15/08/15.
  */
 package object yaml {
-//  def parseJson(in: JsonInput, useBigDecimalForDouble: Boolean = false): JValue =
-//    JsonMethods.parse(in, useBigDecimalForDouble)
-//
-//  def parseJsonOpt(in: JsonInput, useBigDecimalForDouble: Boolean = false): Option[JValue] =
-//    JsonMethods.parseOpt(in, useBigDecimalForDouble)
-//
-//  def renderJValue(value: JValue)(implicit formats: Formats = DefaultFormats): JValue =
-//    JsonMethods.render(value)(formats)
-//
-//  def compactJson(d: JValue): String = JsonMethods.compact(d)
-//
-//  def prettyJson(d: JValue): String = JsonMethods.pretty(d)
+  def parseJson(in: JsonInput, useBigDecimalForDouble: Boolean = false): JValue =
+    YamlMethods.parse(in, useBigDecimalForDouble)
+
+  def parseJsonOpt(in: JsonInput, useBigDecimalForDouble: Boolean = false): Option[JValue] =
+    YamlMethods.parseOpt(in, useBigDecimalForDouble)
+
+  def renderJValue(value: JValue)(implicit formats: Formats = DefaultFormats): JValue =
+    YamlMethods.render(value)(formats)
+
+  def compactJson(d: JValue): String = YamlMethods.compact(d)
+
+  def prettyJson(d: JValue): String = YamlMethods.pretty(d)
 }
